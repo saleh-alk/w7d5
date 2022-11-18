780d3a8072366d501b76fe9026b4f775ab58a863
XiWang <vivian.xi.wang@gmail.com>
2022-11-18 10:47:20 -0800

    Finished Phase 2

--- w7d5/reddit_clone/app/controllers/application_controller.rb
+++ w7d5/reddit_clone/app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-
+    helper_method :current_user, :logged_in?
 
     def current_user
         @current_user ||= User.find_by(session_token: session[:session_token])
     end
 
     def login(user)
-        session[:session_token] = @current_user.reset_session_token!
+        session[:session_token] = user.reset_session_token!
     end
 
     def logged_in?
@@ -17,5 +17,16 @@ class ApplicationController < ActionController::Base
         current_user.reset_session_token! if logged_in?
         session[:session_token]= nil
         @current_user = nil
+    end
+
+    def require_logged_in
+        redirect_to new_session_url unless logged_in?
+            
+    end
+
+
+
+    def require_logged_out
+        redirect_to users_url if logged_in?
     end
 end
---
+++ w7d5/reddit_clone/app/models/sub.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: subs
+#
+#  id           :bigint           not null, primary key
+#  title        :string           not null
+#  description  :text
+#  moderator_id :bigint           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+class Sub < ApplicationRecord
+    validates :title, presence: true
+    # validates :description
+
+    belongs_to :moderator,
+        foreign_key: :moderator_id,
+        class_name: :User
+
+    has_many :posts,
+        foreign_key: :sub_id,
+        class_name: :Post,
+        dependent: :destroy,
+        
+
+
+end
